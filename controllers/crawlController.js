c4b7a435d10ea714029fff36ec3a57290dbc97c6
Fanboyingeneral <2105065@ugrad.cse.buet.ac.bd>
2024-10-17 17:11:23 +0600

    Option to toggle respect_robot.txt added

--- controllers/crawlController.js
+++ controllers/crawlController.js
@@ -2,12 +2,12 @@ const axios = require('axios');
 
 // Controller function to trigger the crawler engine
 const startCrawl = async (req, res) => {
-  const { url, maxUrls } = req.body;
+  const { url, maxUrls, respectRobotFlag } = req.body;
 
   try {
     // Call the crawler engine (Python service) via HTTP request
-    const response = await axios.post('http://crawler_engine:8000/crawl', { url, maxUrls });
-
+    const response = await axios.post('http://crawler_engine:8000/crawl', { url, maxUrls, respectRobotFlag });
+    //console.log('Crawl response:', response.data);
     res.status(200).json(response.data); // Send crawled data back to frontend
   } catch (error) {
     console.error('Error starting crawl:', error.message);
